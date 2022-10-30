91bafbd1963adf3fb2b5c0a0ad532e028df61360
Sameer Kumar <xvisierra@gmail.com>
2022-10-30 19:33:02 +0530

    temp

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Sameer Kumar Singh',
-    title: "Web Developer",
+    title: "Engineer,Software Developer,Web Developer",
     desciption:"Namasakar🙏 I am Sameer a budding Web developer , Software engineer and a proud Engineer.I am currently in 3rd year of my B.E. in RNS Institute of Technology specializing in Information Science.",
-    image: 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/assets/png/p1.jpg?token=GHSAT0AAAAAABZ5IMIEKIJKPLRDXKGKE6JUY26RXOQ',
+    image: 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/assets/png/p1.jpg?token=GHSAT0AAAAAABZ5IMIFRF5MB2YNJBHSCPXYY26R4XQ',
     resumePdf: resume
 }
