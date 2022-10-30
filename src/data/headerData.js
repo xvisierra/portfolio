027e448e8cf04254074bcfe7431f49d0ebb81b4b
Sameer Kumar <xvisierra@gmail.com>
2022-10-30 19:13:27 +0530

    p1 croppping

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Sameer Kumar Singh',
     title: "Web Developer",
     desciption:"Namasakar🙏 I am Sameer a budding Web developer , Software engineer and a proud Engineer.I am currently in 3rd year of my B.E. in RNS Institute of Technology specializing in Information Science.",
-    image: 'https://cdn-icons-png.flaticon.com/512/4128/4128176.png',
+    image: 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/assets/png/p1.jpg?token=GHSAT0AAAAAABZ5IMIFDNVY6LQZRJZHXLQWY26RU3Q',
     resumePdf: resume
 }
