7f2a0db9d8082848fbae3d2ec09fa561b709b87d
Sameer Kumar <xvisierra@gmail.com>
2022-10-30 19:10:19 +0530

    certs ordering

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -3,124 +3,124 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
-            title : 'Crash Course on Python',
-            details : 'an online non-credit course authorized by Google and offered through Coursera',
-            date : 'Jan 27, 2022',
-            field : 'Coding',
-            image : 'https://images.pexels.com/photos/14222030/pexels-photo-14222030.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-        },
-        {
-            id : 2,
-            title : 'Introduction to Cybersecurity Tools & Cyber Attacks',
-            details : 'an online non-credit course authorized by IBM and offered through Coursera',
-            date : 'Jan 2, 2022',
-            field : 'Cyber Security',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-91ae3ea2-39ee-4a57-bf23-d397aff0d9a7.jpg?v=1621951317000'
-        },
-        {
-            id : 3,
-            title : 'HTML, CSS, and Javascript for Web Developers',
-            details : 'an online non-credit course authorized by Johns Hopkins University and offered through Coursera',
-            date : 'Dec 12, 2021',
-            field : 'Web Development',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-d9a7ba99-8635-47e6-96fc-7dd26a3dcbf1.jpg?v=1618297236000'
-        },
-        {
-            id : 4,
-            title : 'Python and Machine Learning',
-            details : 'an online non-credit course offered by Devtown',
-            date : 'AUg 12, 2022',
-            field : 'Machine Learning',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 5,
-            title : 'Global Navigation Satellite System and Location based services' ,
-            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
-            date : 'Mar 15, 2022',
-            field : 'IIRS,ISRO',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 5,
-            title : 'Hyperspectral And Microwave Remote Sensing Techniques For Geological Studies' ,
-            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
-            date : 'Apr 30, 2022',
-            field : 'IIRS,ISRO',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 5,
-            title : 'Machine learning to Deep Learning: A journey for remote sensing data classification' ,
-            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
-            date : 'Jul 19, 2022',
-            field : 'IIRS,ISRO',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 6,
-            title : 'Overview of Web GIS Technology' ,
-            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
-            date : 'Jul 10, 2021',
-            field : 'IIRS,ISRO',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 7,
-            title : 'Satellite based Navigation: A Journey from GPS to Mobile Phone Platform' ,
-            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
-            date : 'Mar 30, 2021',
-            field : 'IIRS,ISRO',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        },
-        {
-            id : 5,
             title : 'Software Engineering Virtual Experience' ,
             details : 'an online non-credit virtual Internship offered by JPMorgan Chase and offered by Forage',
             date : 'Aug 5, 2022',
             field : 'JPMorgan Chase,Forage',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/J.P.%20Morgan%20Software%20Engineering%20Virtual%20Experience-1.jpg?token=GHSAT0AAAAAABZ5IMIEDALRNXFWPA6QRWJUY26Q2KQ'
+        },
+        {
+            id : 2,
+            title : 'Python and Machine Learning',
+            details : 'an online non-credit course offered by Devtown',
+            date : 'AUg 12, 2022',
+            field : 'Machine Learning',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Devtown%20Certificate%20of%20Completion-1.jpg?token=GHSAT0AAAAAABZ5IMIFJDYMDAKUITCZMOG4Y26QU3A'
+        },
+        {
+            id : 3,
+            title : 'Global Navigation Satellite System and Location based services' ,
+            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
+            date : 'Mar 15, 2022',
+            field : 'IIRS,ISRO',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Global%20Navigation%20Satellite%20System%20and%20Location%20based%20services%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIEO5ORO4J5GKH64XNIY26QVSQ'
+        },
+        {
+            id : 4,
+            title : 'Hyperspectral And Microwave Remote Sensing Techniques For Geological Studies' ,
+            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
+            date : 'Apr 30, 2022',
+            field : 'IIRS,ISRO',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Hyperspectral%20And%20Microwave%20Remote%20Sensing%20Techniques%20For%20Geological%20Studies-1.jpg?token=GHSAT0AAAAAABZ5IMIEMCU5LQ4AWOHZGZEIY26QWPA'
+        },
+        {
+            id : 5,
+            title : 'Crash Course on Python',
+            details : 'an online non-credit course authorized by Google and offered through Coursera',
+            date : 'Jan 27, 2022',
+            field : 'Coding,Python',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera%20-%20Crash%20course%20on%20Python-1.jpg?token=GHSAT0AAAAAABZ5IMIFORFASAD7NUCBTXJUY26QS7A'
         },
         {
             id : 6,
+            title : 'Introduction to Cybersecurity Tools & Cyber Attacks',
+            details : 'an online non-credit course authorized by IBM and offered through Coursera',
+            date : 'Jan 2, 2022',
+            field : 'Cyber Security',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera-Introduction%20to%20Cybersecurity%20Tools%20%26%20Cyber%20Attacks-1.jpg?token=GHSAT0AAAAAABZ5IMIEDUD6DOB2PFWYG45IY26QTUQ'
+        },
+        {
+            id : 7,
+            title : 'HTML, CSS, and Javascript for Web Developers',
+            details : 'an online non-credit course authorized by Johns Hopkins University and offered through Coursera',
+            date : 'Dec 12, 2021',
+            field : 'Web Development',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera%20-%20HTML%2C%20CSS%2C%20and%20Javascript%20for%20Web%20Developers-1.jpg?token=GHSAT0AAAAAABZ5IMIEOLU3KX6EP34MQH64Y26QUBA'
+        },
+        {
+            id : 8,
             title : 'Python Programming' ,
             details : 'an online non-credit course offered by Great Learning',
             date : 'Dec, 2021',
             field : 'Great Learning',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Python%20Programming%20-%20Great%20Learning%20Academy-1.jpg?token=GHSAT0AAAAAABZ5IMIFIHADYSU7OVHTMOKMY26Q24Q'
         },
         {
-            id : 7,
+            id : 9,
             title : '5-day Workshop on Advanced C Programming with Placement Perspective' ,
             details : 'an online 5 day Workshop organized by RNSIT',
             date : 'Oct 12, 2021',
             field : 'RNSIT',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/5%20days%20workshop%20on%20programming%20in%20C%20-%20RNSIT-1.jpg?token=GHSAT0AAAAAABZ5IMIE7UL7AL7EZM4ZFDREY26Q3TA'
         },
         {
-            id : 7,
+            id : 10,
             title : 'Learn and Master C Programming For Absolute Beginners!' ,
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 20, 2021',
             field : 'Infosys Springboard',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-%20Learn%20and%20Master%20C%20Programming%20For%20Absolute%20Beginners-1.jpg?token=GHSAT0AAAAAABZ5IMIEA3AOJKUQTJLNQ3FKY26Q4EQ  '
         },
         {
-            id : 7,
+            id : 11,
             title : 'Speak Up' ,
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 8, 2021',
             field : 'Infosys Springboard',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-Speak%20Up-1.jpg?token=GHSAT0AAAAAABZ5IMIFG2L6VKDXRLMCLXRYY26Q4RA'
         },
         {
-            id : 7,
+            id : 12,
             title : 'Understanding Social Styles' ,
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 18, 2021',
             field : 'Infosys Springboard',
-            image : 'https://udemy-certificate.s3.amazonaws.com/image/UC-8f5ff8e8-b6a4-4efd-a3fc-7fb63b29e77f.jpg?v=1614489452000'
-        }
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-Understanding%20Social%20Styles-1.jpg?token=GHSAT0AAAAAABZ5IMIE2IADICRCIIJUVMEIY26Q5VA'
+        },
+        {
+            id : 13,
+            title : 'Machine learning to Deep Learning: A journey for remote sensing data classification' ,
+            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
+            date : 'Jul 19, 2022',
+            field : 'IIRS,ISRO',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Machine%20learning%20to%20Deep%20Learning%20A%20journey%20for%20remote%20sensing%20data%20classification%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIFX6ME4QYQA537725WY26QXTQ'
+        },
+        {
+            id : 14,
+            title : 'Overview of Web GIS Technology' ,
+            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
+            date : 'Jul 10, 2021',
+            field : 'IIRS,ISRO',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Overview%20of%20Web%20GIS%20Technology%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIFIZPKIWQ3U35UDLWYY26QYEA'
+        },
+        {
+            id : 15,
+            title : 'Satellite based Navigation: A Journey from GPS to Mobile Phone Platform' ,
+            details : 'an online non-credit course offered by IIRS and authorized by ISRO',
+            date : 'Mar 30, 2021',
+            field : 'IIRS,ISRO',
+            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Satellite%20based%20Navigation%20A%20Journey%20from%20GPS%20to%20Mobile%20Phone%20Platform-1.jpg?token=GHSAT0AAAAAABZ5IMIFVHXZONGZ65OQC3UEY26QZ3Q '
+        },
 
     ]
 }
