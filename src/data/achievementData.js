c7371bd182c752fda60564cc8c50de4608408aeb
Sameer Kumar <xvisierra@gmail.com>
2022-10-31 18:27:56 +0530

    pic source changed from github to imgur

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -7,7 +7,7 @@ export const achievementData = {
             details : 'an online non-credit virtual Internship offered by JPMorgan Chase and offered by Forage',
             date : 'Aug 5, 2022',
             field : 'JPMorgan Chase,Forage',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/J.P.%20Morgan%20Software%20Engineering%20Virtual%20Experience-1.jpg?token=GHSAT0AAAAAABZ5IMIEDALRNXFWPA6QRWJUY26Q2KQ'
+            image : 'https://i.imgur.com/VRy1YF2.jpg'
         },
         {
             id : 2,
@@ -15,7 +15,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by Devtown',
             date : 'AUg 12, 2022',
             field : 'Machine Learning',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Devtown%20Certificate%20of%20Completion-1.jpg?token=GHSAT0AAAAAABZ5IMIFJDYMDAKUITCZMOG4Y26QU3A'
+            image : 'https://i.imgur.com/Zn3iCmF.jpg'
         },
         {
             id : 3,
@@ -23,7 +23,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by IIRS and authorized by ISRO',
             date : 'Mar 15, 2022',
             field : 'IIRS,ISRO',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Global%20Navigation%20Satellite%20System%20and%20Location%20based%20services%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIEO5ORO4J5GKH64XNIY26QVSQ'
+            image : 'https://i.imgur.com/YIr6tQr.jpg'
         },
         {
             id : 4,
@@ -31,7 +31,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by IIRS and authorized by ISRO',
             date : 'Apr 30, 2022',
             field : 'IIRS,ISRO',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Hyperspectral%20And%20Microwave%20Remote%20Sensing%20Techniques%20For%20Geological%20Studies-1.jpg?token=GHSAT0AAAAAABZ5IMIEMCU5LQ4AWOHZGZEIY26QWPA'
+            image : 'https://i.imgur.com/sQ9jI1m.jpg'
         },
         {
             id : 5,
@@ -39,7 +39,7 @@ export const achievementData = {
             details : 'an online non-credit course authorized by Google and offered through Coursera',
             date : 'Jan 27, 2022',
             field : 'Coding,Python',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera%20-%20Crash%20course%20on%20Python-1.jpg?token=GHSAT0AAAAAABZ5IMIFORFASAD7NUCBTXJUY26QS7A'
+            image : 'https://i.imgur.com/B5KDrcg.jpg'
         },
         {
             id : 6,
@@ -47,7 +47,7 @@ export const achievementData = {
             details : 'an online non-credit course authorized by IBM and offered through Coursera',
             date : 'Jan 2, 2022',
             field : 'Cyber Security',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera-Introduction%20to%20Cybersecurity%20Tools%20%26%20Cyber%20Attacks-1.jpg?token=GHSAT0AAAAAABZ5IMIEDUD6DOB2PFWYG45IY26QTUQ'
+            image : 'https://i.imgur.com/XI2Ndtz.jpg'
         },
         {
             id : 7,
@@ -55,7 +55,7 @@ export const achievementData = {
             details : 'an online non-credit course authorized by Johns Hopkins University and offered through Coursera',
             date : 'Dec 12, 2021',
             field : 'Web Development',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Coursera%20-%20HTML%2C%20CSS%2C%20and%20Javascript%20for%20Web%20Developers-1.jpg?token=GHSAT0AAAAAABZ5IMIEOLU3KX6EP34MQH64Y26QUBA'
+            image : 'https://i.imgur.com/8ct72Yb.jpg'
         },
         {
             id : 8,
@@ -63,7 +63,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by Great Learning',
             date : 'Dec, 2021',
             field : 'Great Learning',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Python%20Programming%20-%20Great%20Learning%20Academy-1.jpg?token=GHSAT0AAAAAABZ5IMIFIHADYSU7OVHTMOKMY26Q24Q'
+            image : 'https://i.imgur.com/9CMdswl.jpg'
         },
         {
             id : 9,
@@ -71,7 +71,7 @@ export const achievementData = {
             details : 'an online 5 day Workshop organized by RNSIT',
             date : 'Oct 12, 2021',
             field : 'RNSIT',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/5%20days%20workshop%20on%20programming%20in%20C%20-%20RNSIT-1.jpg?token=GHSAT0AAAAAABZ5IMIE7UL7AL7EZM4ZFDREY26Q3TA'
+            image : 'https://i.imgur.com/oOOUD4Yh.jpg'
         },
         {
             id : 10,
@@ -79,7 +79,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 20, 2021',
             field : 'Infosys Springboard',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-%20Learn%20and%20Master%20C%20Programming%20For%20Absolute%20Beginners-1.jpg?token=GHSAT0AAAAAABZ5IMIEA3AOJKUQTJLNQ3FKY26Q4EQ  '
+            image : 'https://i.imgur.com/oOOUD4Y.jpg'
         },
         {
             id : 11,
@@ -87,7 +87,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 8, 2021',
             field : 'Infosys Springboard',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-Speak%20Up-1.jpg?token=GHSAT0AAAAAABZ5IMIFG2L6VKDXRLMCLXRYY26Q4RA'
+            image : 'https://i.imgur.com/dmmokdU.jpg'
         },
         {
             id : 12,
@@ -95,7 +95,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by Infosys SpringBoard',
             date : 'Sep 18, 2021',
             field : 'Infosys Springboard',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/Infosys%20Spring%20Board-Understanding%20Social%20Styles-1.jpg?token=GHSAT0AAAAAABZ5IMIE2IADICRCIIJUVMEIY26Q5VA'
+            image : 'https://i.imgur.com/ymlKsS1.jpg'
         },
         {
             id : 13,
@@ -103,7 +103,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by IIRS and authorized by ISRO',
             date : 'Jul 19, 2022',
             field : 'IIRS,ISRO',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Machine%20learning%20to%20Deep%20Learning%20A%20journey%20for%20remote%20sensing%20data%20classification%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIFX6ME4QYQA537725WY26QXTQ'
+            image : 'https://i.imgur.com/K8qASnP.jpg'
         },
         {
             id : 14,
@@ -111,7 +111,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by IIRS and authorized by ISRO',
             date : 'Jul 10, 2021',
             field : 'IIRS,ISRO',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Overview%20of%20Web%20GIS%20Technology%20(SIGNED)-1.jpg?token=GHSAT0AAAAAABZ5IMIFIZPKIWQ3U35UDLWYY26QYEA'
+            image : 'https://i.imgur.com/VITN7XAh.jpg'
         },
         {
             id : 15,
@@ -119,7 +119,7 @@ export const achievementData = {
             details : 'an online non-credit course offered by IIRS and authorized by ISRO',
             date : 'Mar 30, 2021',
             field : 'IIRS,ISRO',
-            image : 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/components/Achievement/certificates/IIRS%20ISRO%20Satellite%20based%20Navigation%20A%20Journey%20from%20GPS%20to%20Mobile%20Phone%20Platform-1.jpg?token=GHSAT0AAAAAABZ5IMIFVHXZONGZ65OQC3UEY26QZ3Q '
+            image : 'https://i.imgur.com/Kl9oggY.jpg'
         },
 
     ]
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Sameer Kumar Singh',
-    title: "Engineer,Software Developer,Web Developer",
+    title: "Web Developer",
     desciption:"Namasakar🙏 I am Sameer a budding Web developer , Software engineer and a proud Engineer.I am currently in 3rd year of my B.E. in RNS Institute of Technology specializing in Information Science.",
-    image: 'https://raw.githubusercontent.com/xvisierra/portfolio/main/src/assets/png/p1.jpg?token=GHSAT0AAAAAABZ5IMIFRF5MB2YNJBHSCPXYY26R4XQ',
+    image: 'https://i.imgur.com/HHCB6Ur.jpg',
     resumePdf: resume
 }
